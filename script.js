2082dd9e7df69230be2bb1f34224afb4db6136bb
leandroTokudome <leandro.tokudome@sptech.school>
2023-12-10 21:51:41 -0300

    Começando

---
+++ script.js
@@ -0,0 +1,14 @@
+varPlayerMaxHp = 10
+varPlayerHp = 10
+varPlayerMaxMana = 10
+varPlayerMana = 10
+varPlayerGold = 0
+varPlayerLvl = 1
+varPlayerExp = 0
+varPlayerItens = []
+varPlayerSkills = []
+varPlayerPassives = []
+
+function updateCharacterStatus() {
+
+}
