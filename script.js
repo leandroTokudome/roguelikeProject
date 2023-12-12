fae0136905e517411fea8de4f5750d51f0d4adae
leandroTokudome <leandro.tokudome@sptech.school>
2023-12-11 21:25:52 -0300

    Poucas coisas

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ varPlayerMaxHp = 10
 varPlayerHp = 10
 varPlayerMaxMana = 10
 varPlayerMana = 10
+varPlayerSpecial = 0
 varPlayerGold = 0
 varPlayerLvl = 1
 varPlayerExp = 0
+varPlayerMaxExp = 5
 varPlayerItens = []
 varPlayerSkills = []
 varPlayerPassives = []
 
 function updateCharacterStatus() {
 
+}
+
+function levelUp() {
+
+}
+
+function generateChoices() {
+    
 }
